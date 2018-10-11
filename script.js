3355a06f87ab0d8856e22808284768199c12e320
Meghana Joshi <meghana@thinkful.commgit config --global user.email meghana@thinkful.com>
2018-10-11 14:44:50 -0400

    made changes now we can navigate from one page to other

--- script.js
+++ script.js
@@ -1,27 +1,133 @@
-// initialize the correct answers using session storage API
+const store = {
+	'questions': [{
+		'question': 'What does Ariel collect?',
+		'answers': ['Sea shells', 'eels', 'human items', 'seaweed'],
+		'correct': 2
+	},
+	{
+		'question': 'How many stepsisters does cinderella have?',
+		'answers': ['Two', 'Three', 'Eight', 'one'],
+		'correct': 0
+	},
+	{
+		'question': 'What is the name of the mouse Cinderella rescues?',
+		'answers': ['Vinny', 'Chad', 'Gus', 'Jackie'],
+		'correct': 2
+	},
+	{
+		'question': 'What was Dory\'s first line in the movie Finding Dory?',
+		'answers': ['Look Out', 'Hi, I am Dory', 'Just keep swimming', 'Hey there Mr. Grumpy Grills'],
+		'correct': 2
+	},
+	{
+		'question': 'What does Ariel collect?',
+		'answers': ['Sea shells', 'eels', 'human items', 'seaweed'],
+		'correct': 2
+	},
+
+	{
+		'question': 'What is the name of Micky Mouse\'s dog?',
+		'answers': ['Mars', 'Neptune', 'Goofy', 'Pluto'],
+		'correct': 3
+	}
+	],
+	'score': 0,
+	'currentIndex': 0,
+	'isStarted': false
+};
+function generateStartPageHTML() {
+	return ` <div id = 'container'>
+		<header role =banner class ='startQuiz' >
+		<h1> Welcome to the  Disney movies Quiz!! </h1>
+	</header>
+		<div class = 'start'>
+		<p > How well do you know your Disney movies?</p><hr>
+		<button class='start-quiz'>START QUIZ</button>
+		</div>
+		</div>`;
+}
+function handleStartQuiz() {
+	console.log("start handle clicked");
+	$('.app').on('click' ,'.start-quiz', function () {
+		store.isStarted = true;
+		store.currentIndex = 0;
+		store.score = 0;
+		render();
+	});
+}
+
+function handleQuestionSubmit() {
+	$('.app').on('submit', ".questionForm",function (event) {
+
+		event.preventDefault();
+		console.log("nextQuestion clicked");
+		store.currentIndex = store.currentIndex + 1;	
+		render();
+
+	})
+}
+function render() {
+	console.log("Render was called")
+	const isFinished = store.currentIndex >= store.questions.length;
+	if(!store.isStarted){
+		renderStartPage();
+	}
+	else if (isFinished) {
+		renderResult();
+	}
+	else  {
+		renderQuestion();
+
+	}
+
+	
+}
+function renderStartPage() {
+	$(".app").html(generateStartPageHTML());
+
+}
+function renderQuestion() {
+
+	let currentQuestion = store.questions[store.currentIndex];
+
+	$('.app').html(generateQuestionHTML(currentQuestion));
+
+}
+function renderResult(){
+	$('.app').html(generateResultHTML());
+}
 
 
-	let score = 0;// set the score to 0
-	let total  = 0 ; // total number of questions
 
-	// function init(){
-	// 	sessionStorage.setItem('a1','A');
-	// 	sessionStorage.setItem('a2','C');
-	// 	sessionStorage.setItem('a3','A');
-	// 	sessionStorage.setItem('a4','D');
-	// 	sessionStorage.setItem('a5','B');
-	// 	sessionStorage.setItem('a6','D');
+function generateQuestionHTML(question) {
+	$('.app').html(`<div class="container">
+				<header>
+				<div class ="pull-left">The Disney Quiz</div>
+				<div class ="pull-right">By - Meghana J.</div>
+				</header>
+				<form action="post" class = "questionForm" >
 
-	// }
+						<h3> ${question.question}</h3>
+						<ul>
+						${question.answers.map(function (answer, index) {
+			return `<li> <input id="ans-${index}" type="radio" name ='answers'  value = '${index}' required > 
+							<label for="ans-${index}">${answer}</label></input></li>`;
+		}).join('')}
+						
+						</ul>
 
-	function init(){
-	//set correct answers
-	sessionStorage.setItem('a1','A');
-	sessionStorage.setItem('a2','C');
-	sessionStorage.setItem('a3','A');
-	sessionStorage.setItem('a4','D');
-	sessionStorage.setItem('a5','B');
-	sessionStorage.setItem('a6','D');
+						<button id ='submitButton' type = "submit" > Submit </button>
+					</form>
+			</form>
+			 <p id="scoretracker">Your score is <span id = 'currentScore'> </span> ${store.score} / 6 </p>
+
+			</div>`
+	)
+};
+
+function generateResultHTML(){
+	return (`<h1>Your score is  ${store.score} </h1>
+	<p><button id ='restart' type = "submit" > Restart </button></p>`);
 }
 
 
@@ -29,216 +135,19 @@
 
 
 
+
+function loadAnswers() {
+}
+function displayCurrentScore() {
+}
+function displayResults() { }
+
+function main() {
+	handleStartQuiz();
+	handleQuestionSubmit();
+	render();
+}
 $(
-	function(){
-		//hide all the questions
-		$('.questionForm').hide();
-		//show the start page
-		$('#startPage').show();
-		//on submit show the second page
-
-		$('#startPage').on('click','#submit',function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-
-
-			$('#Q1').fadeIn(300);
-
-
-		});
-
-
-
-		$('#Q1 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			processor('Q1');
-			$("#scoreKeeper").text(score);
-			
-
-			$('#Q2').fadeIn(300);
-
-
-		});
-
-		$('#Q2 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('#scoreTeller').text('');
-			$('.questionForm').hide();
-			processor('Q2');
-			$("#scoreKeeper").text(score);
-			$('#Q3').fadeIn(300);
-
-		});
-
-		$('#Q3 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			processor('Q3');
-			$("#scoreKeeper").text(score);
-			$('#Q4').fadeIn(300);
-
-		});
-
-		$('#Q4 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			processor('Q4');
-			$("#scoreKeeper").text(score);
-			$('#Q5').fadeIn(300);
-
-		});
-		$('#Q5 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			processor('Q5');
-			$("#scoreKeeper").text(score);
-			$('#Q6').fadeIn(300);
-
-		});
-		$('#Q6 #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			processor("Q6");
-			$("#scoreKeeper").text(score);
-
-		$('#resultPage').fadeIn(300);
-
-
-		});
-		$('#resultPage #submit').click(function(event)
-		{
-			event.preventDefault();
-			$('.questionForm').hide();
-			score =0;
-
-			$('#Q1').fadeIn(300);
-			return false;
-
-
-		});
-
-
-
-	});
-//process the answers
-function processor(q){
-	if (q === 'Q1'){
-		let submitted = $('input[name =Q1]:checked').val();
-		if (submitted === sessionStorage.a1){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-	}
-		if (q === 'Q2'){
-		let submitted = $('input[name =Q2]:checked').val();
-		if (submitted === sessionStorage.a2){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-
-		}
-
-	}
-
-
-	if (q === 'Q3'){
-		let submitted = $('input[name =Q3]:checked').val();
-		if (submitted === sessionStorage.a3){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-
-		}
-
-	
-
-	if (q === 'Q4'){
-		let submitted = $('input[name =Q4]:checked').val();
-		if (submitted === sessionStorage.a4){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-
-		}
-
-	
-
-	if (q === 'Q5'){
-		let submitted = $('input[name =Q5]:checked').val();
-		if (submitted === sessionStorage.a5){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-
-		}
-
-	
-
-
-	if (q === 'Q6'){
-		let submitted = $('input[name =Q6]:checked').val();
-		if (submitted === sessionStorage.a6){
-			score++;
-			$('#scoreTeller').append(". You are correct");
-
-		}
-		else 
-			$('#scoreTeller').append("  You are Wrong");
-
-		
-
-
-		}
-
-
-
-
-
-	
-
-
-
-window.addEventListener('load',init,false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	main
+);
 
